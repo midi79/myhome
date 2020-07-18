fcb9b6912b61d97b24d2312ab3a018192e41b0c2
midi79 <midi79@naver.com>
2020-07-18 22:39:52 +0900

    Add files via upload

---
+++ src/test/java/com/midi279/myhome/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midi279.myhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
